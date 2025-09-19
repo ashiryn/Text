b01fa52d6faad654ee881c7510baa8c0fc974923
ashiryn <ashiryn@users.noreply.github.com>
2025-09-19 17:01:24 -0400

    Updated to properly pack xml docmentation file (#4)

--- Text/AutoCompletion/DefaultAutoCompleteData.cs
+++ Text/AutoCompletion/DefaultAutoCompleteData.cs
@@ -9,9 +9,9 @@ public class DefaultAutoCompleteData
     /// <summary>
     ///     The keyword that will be used to calculate if there are any suggestions available for a text
     /// </summary>
-    public required string Keyword { get; set; }
+    public string? Keyword { get; set; }
     /// <summary>
     ///     List of suggestions available for this keyword
     /// </summary>
-    public required string[] Suggestions { get; set; }
+    public string[]? Suggestions { get; set; }
 }
